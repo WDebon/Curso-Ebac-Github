0ef5c49397fa29a512158c58286abd9bb1bb1c20
White Debon <38989015+WDebon@users.noreply.github.com>
2023-08-12 02:21:03 -0300

    adicionando keyup e organizando + animação reversa

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const InputA = document.getElementById('imA');
 const InputB = document.getElementById('imB');
 const erro = document.querySelectorAll(".inp"); //qsAll cria uma ''string'' (Nodelist) então para afetar todos os selecionados temos que tratalo como uma
-const butN = document.getElementById("form");
+const varForm = document.getElementById("idForm");
+const butOn = document.querySelector("button");
 const menOr = document.querySelector(".men"); // querySelector deixa você pegar qualquer parte do html
 
 menOr.classList.remove("men")
@@ -14,29 +15,55 @@ function comparando(A, B) {
 }
 
 //function(e) é porque estamos pegando o evento para alterar, no caso o submit
-butN.addEventListener('submit', function (e) {
-
-    if(comparando(InputA.valueAsNumber,InputB.valueAsNumber)){
-        alert("Deu certo!")
-
-        menOr.classList.remove("men")
-
-        for(var i=0; i<erro.length; i++){
-            erro[i].style.display = "none"
-            erro[i].classList.remove("exp")
-        }
-        // erroa.style.display = "none"
-    }else{
-
-        menOr.classList.add("men")
-        
-        for(var i=0; i<erro.length; i++){
-            erro[i].style.display = "block"
-            erro[i].classList.add("exp")
-        }
-        
+varForm.addEventListener('submit', function (e) {
+    if (!comparando(InputA.valueAsNumber, InputB.valueAsNumber)){
         e.preventDefault() //vai parar do evento fazer o que ele geralmente faz
-        // errob.style.display = "block"
     }
+})
 
+varForm.addEventListener("keyup", function(e){
+        if (comparando(InputA.valueAsNumber, InputB.valueAsNumber)) {
+
+            menOr.classList.remove("men")
+            menOr.classList.add("men2")
+    
+            for (var i = 0; i < erro.length; i++) {
+                erro[i].style.display = "none"
+                erro[i].classList.remove("exp")
+            }
+            
+            butOn.style.backgroundColor = "#3eaa6e"
+
+            InputA.style.border = "thin dashed green"
+            InputB.style.border = "thin dashed green"
+            InputA.style.backgroundColor = "#bbe2cd"
+            InputB.style.backgroundColor = "#bbe2cd"
+    
+            InputA.style.outlineColor = "green"
+            InputB.style.outlineColor = "green"
+    
+            // erroa.style.display = "none"
+        } else {
+    
+            menOr.classList.add("men")
+            menOr.classList.remove("men2")
+    
+            for (var i = 0; i < erro.length; i++) {
+                erro[i].style.display = "block"
+                erro[i].classList.add("exp")
+            }
+            
+            butOn.style.backgroundColor = "grey"
+
+            InputA.style.border = " thin dashed red "
+            InputB.style.border = " thin dashed red "
+            InputA.style.background = "#f1c4c4"
+            InputB.style.background = "#f1c4c4"
+    
+            InputA. style.outlineColor = "red"
+            InputB.style.outlineColor = "red"
+
+            // errob.style.display = "block"
+        
+    }
 })
