4ab57d1a071b4d2a9b8cfdff51f24586c2587561
White Debon <38989015+WDebon@users.noreply.github.com>
2023-08-24 13:44:09 -0300

    - JS para selecionar /adicionar e mudar o LI

--- main.js
+++ main.js
@@ -5,29 +5,38 @@ const date = new Date()
 const year = date.getFullYear()
 const day = date.getDate()
 const month = date.getMonth() 
+var current_Date = "["+day+"/"+month+"/"+year+"]";
+console.log(current_Date)
 // const milli = date.getTime()
 // let seconds = Math.trunc(milli/1000)
 // let minutes = Math.trunc(seconds/60)
 // let hour = Math.trunc(minutes/60)
 // var time = hour+":"+minutes+":"+seconds
-var current_Date = "["+day+"/"+month+"/"+year+"]";
-
-console.log(current_Date)
 
 function write(at,dat,cdat){
 
+    let lili = `<li class="off">`;
+
     let taf = $(
-        ` <li class="off">
+        ` ${lili}
         <span><img src="./Red check.png"></span>
         <span class="txt">${at.val()}</span>
-        <span class="date">${dat.val()}</span>
+        <span class="date">${dat}</span>
             </li>`
         ).appendTo('ul');
 
-        const tafLi = $('li')
+        let dat_Area = $(`<span class="date">`)
 
         $(taf).click(function (e) { 
-            tafLi.
+            if(e.target == $('li')||$('span')||$('img')){
+                $(this).removeClass("off")
+                $(this).addClass("on")
+                $('img',this).attr("src", "Green check.png")
+                $('.date', this).replaceWith(`<span class="date">${cdat}</span>`);
+            }
+            console.log("clicoufunc")
+            console.log($(this))
+            console.log(e.target)
         });
 
         
@@ -43,26 +52,13 @@ console.log($('#butn'))
 $('form').submit(function (e) { 
     // const li = $('ul')
     e.preventDefault();
-
-    
-    
-
         // li.appendTo('body')
-    
-        console.log("foi")
-    
+
+        console.log(added_Date)
+        write(atividade,added_Date,current_Date)  
         atividade.val("")
         added_Date.val("") 
 });
-
-$(classOff).click(function (e) { 
-    console.log("clicou")
-    // $(`<li class="on"><span><img src="./green check.png"></span><span class="txt">${atividade.val()}</span><span class="date">${current_Date.val()}</span></li>`).appendTo(body);
-});
-
-
-
-
 
 // function Stringing(func){
 //     const splitString = func.split(" ");
