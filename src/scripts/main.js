70224e2f2cf88b4fe9ecee3c91bcdd55d057f5d4
WDebon <robielfelix@gmail.com>
2025-01-06 16:47:37 -0300

    + Java

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,14 @@
-document.addEventListener("DOMConentloaded",function(){
+document.addEventListener("DOMConentloaded",function(e){
+    e.preventDefault();
     document.getElementById("form-sorted").addEventListener("submit",function(){
         let maxNumber = document.getElementById("maxNumber").value;
         maxNumber = parseInt(maxNumber);
 
         let randomNumber = Math.random() * maxNumber;
+        randomNumber = Math.floor(randomNumber + 1)
         // alert(randomNumber)
+
+        document.getElementById("result-number").innerText = randomNumber;
+        document.querySelector(".result").style.display = "block";
     })
 })
