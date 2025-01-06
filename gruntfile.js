6780231903ee076264c05fd0fcba8df6460b9438
WDebon <robielfelix@gmail.com>
2025-01-06 18:56:35 -0300

    + abonitando - clean

--- gruntfile.js
+++ gruntfile.js
@@ -2,43 +2,25 @@ const { file, option } = require("grunt");
 
 module.exports = function(grunt){
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        //Os tasks a seguir poderiam ter qualquer outro nome dentro que n mudaria nada (Trocar "Files" por "f" = mesmo resultado)
-        less: {
-            development:{
-                files:{
-                    './build/styles/main.css':'./src/styles/main.less'
-                }
-            }
-        },
-
-        replace:{
+        pkg: grunt.file.readJSON('package.json')
+        ,
+        less: {development:{files:{'./build/styles/main.css':'./src/styles/main.less'}}} //Compilando LESS
+        ,
+        replace:{ 
             dev:{
-                options:{
-                    patterns:[
-                        {match: 'css',replacement: './styles/main.css'},
-                        {match: 'js',replacement: './scripts/main.js'}
-                    ]
-                },
+                options:{patterns:[{match: 'css',replacement: './styles/main.css'},{match: 'js',replacement: './scripts/main.js'}]}
+                ,
                 files: [{expand: true, flatten: true, src: ['src/index.html'], dest: 'build/'}]
             }
-        },
-
-        uglify:{
-            target:{
-                files:{
-                    './build/scripts/main.js': './src/scripts/main.js'
-                },
-            }
-        },
-
-        clean:['prebuild']
-
+        } //Trocando os @@ do HTML e colocando ele na ./build
+        ,
+        uglify:{target:{files:{'./build/scripts/main.js': './src/scripts/main.js'},}} //Comprimindo o .js
     })
 
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-replace');
+
 
     grunt.registerTask('default', ['less','uglify','replace'])
 }
