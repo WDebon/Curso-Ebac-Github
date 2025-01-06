6f8a81f32fd2486b4dd66564969f18d6ecd0d9cf
WDebon <robielfelix@gmail.com>
2025-01-06 18:53:56 -0300

    + Mudando tudo

--- gruntfile.js
+++ gruntfile.js
@@ -1,115 +1,44 @@
 const { file, option } = require("grunt");
-const { watch, src, dest } = require("gulp");
 
 module.exports = function(grunt){
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'), 
+        pkg: grunt.file.readJSON('package.json'),
         //Os tasks a seguir poderiam ter qualquer outro nome dentro que n mudaria nada (Trocar "Files" por "f" = mesmo resultado)
         less: {
             development:{
                 files:{
-                    './dev/styles/main.css':'./src/styles/main.less'
+                    './build/styles/main.css':'./src/styles/main.less'
                 }
-            },
-            production:{
-                options:{
-                    compress:true,
-                },
-                files:{
-                    './dist/styles/main.min.css':'./src/styles/main.less'
-                }
-            }
-        },
-
-        watch: {
-            less: { //Watching o Less
-                files:['src/styles/**/*'], //"**" = pastas || "*" = arquivo
-                tasks:['less:development']
-            },
-            html:{ //Watchign o HTML sendo trocado
-                files:['src/index.html'],
-                tasks:['replace:dev']
             }
         },
 
         replace:{
-            dev:{ //Trocando o .js e .css do HTML para o .js e .css normal "main" criado
+            dev:{
                 options:{
                     patterns:[
-                        {
-                            match: 'css_Dest', //usar @@ no links em vez do path
-                            replacement: './styles/main.css'
-                        },
-                        {
-                            match: 'js_Dest', //usar @@ no script em vez do path
-                            replacement: '../src/scripts/main.js'
-                        }
+                        {match: 'css',replacement: './styles/main.css'},
+                        {match: 'js',replacement: './scripts/main.js'}
                     ]
                 },
-                files:[
-                    {
-                        expand: true,
-                        flatten:true,
-                        src:['./src/index.html'],
-                        dest:'dev/'
-                    }
-                ]
-            },
-            dist:{ //Trocando o .css do HTML-minimizado para o .css-minimizado
-                options:{
-                    patterns:[
-                        {
-                            match: 'css_Dest',
-                            replacement: './styles/main.min.css'
-                        },
-                        {
-                            match: 'js_Dest', //usar @@ no script em vez do path
-                            replacement: './scripts/main.min.js'
-                        }
-                    ]
-                },
-                files:[ //colocando o html-minimizado no dist
-                    {
-                        expand: true,
-                        flatten:true,
-                        src:['./prebuild/index.html'],
-                        dest:'dist/'
-                    }
-                ]
-            }
-        },
-
-        htmlmin:{ //Minimizando o html
-            dist:{
-                options:{
-                    removeComments:true,
-                    collapseWhitespace:true,
-                },
-                files:{ //Criando o arquivo prebuild temporariamente para armazenizar o hml minimizado
-                    './prebuild/index.html':'./src/index.html'
-                }
+                files: [{expand: true, flatten: true, src: ['src/index.html'], dest: 'build/'}]
             }
         },
 
         uglify:{
             target:{
                 files:{
-                    './dist/scripts/main.min.js': './src/scripts/main.js'
+                    './build/scripts/main.js': './src/scripts/main.js'
                 },
             }
         },
 
-        clean:['prebuild'] //deletando o prebuild
+        clean:['prebuild']
 
     })
 
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-replace');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['watch'])
-    grunt.registerTask('build', ['less:production','htmlmin:dist','replace:dist','clean','uglify'])
+    grunt.registerTask('default', ['less','uglify','replace'])
 }
