a143419299cf3bf2605ec22c0e8430d7ca7bf77c
WDebon <robielfelix@gmail.com>
2025-01-14 17:35:18 -0300

    + Tudo!

--- main.js
+++ main.js
@@ -1,70 +1,34 @@
-// Example of fetching GitHub profile data using AJAX (XMLHttpRequest)
-
-document.addEventListener('DOMContentLoaded', function() {
-
-        const nameElement = document.querySelector('#name');
-        const usernameElement = document.querySelector('#username');
-        const avatarElement = document.querySelector('#avatar');
-        const reposElement = document.querySelector('#repos');
-        const followersElement = document.querySelector('#followers');
-        const followingElement = document.querySelector('#following');
-        const linkElement = document.querySelector('#profile-link');
-
+document.addEventListener('DOMContentLoaded', function () {
     try {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://api.github.com/users/maxh33', true);
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                const data = JSON.parse(xhr.responseText);
-                if (nameElement) nameElement.innerText = data.name;
-                if (usernameElement) usernameElement.innerText = data.login;
-                if (avatarElement) avatarElement.src = data.avatar_url;
-                if (followingElement) followingElement.innerText = data.following;
-                if (followersElement) followersElement.innerText = data.followers;
-                if (reposElement) reposElement.innerText = data.public_repos;
-                if (linkElement) linkElement.href = data.html_url;
-            } else if (xhr.readyState === 4) {
-                console.error('Error fetching data:', xhr.statusText);
-            }
-        };
-        xhr.send();
-    } catch (error) {
-        console.error('Error fetching data:', error.message);
+        fetch('https://api.github.com/users/WDebon')
+            .then(response => response.json())
+            .then(data => {
+                const nameElement = document.querySelector('#name');
+                const usernameElement = document.querySelector('#username');
+                const avatarElement = document.querySelector('#avatar');
+                const reposElement = document.querySelector('#repos');
+                const followersElement = document.querySelector('#followers');
+                const followingElement = document.querySelector('#following');
+                const blogElement = document.querySelector('#blog');
+                const companyElement = document.querySelector('#company');
+                const locationElement = document.querySelector('#location');
+                const bioElement = document.querySelector('#bio');
+                const linkElement = document.querySelector('#link');
+
+                nameElement.innerText = data.name;
+                usernameElement.innerText = data.login;
+                avatarElement.src = data.avatar_url;
+                followingElement.innerText = data.following;
+                followersElement.innerText = data.followers;
+                reposElement.innerText = data.public_repos;
+                blogElement.href = data.blog;
+                companyElement.innerText += data.company;
+                locationElement.innerText += data.location;
+                bioElement.innerText += data.bio;
+                linkElement.href = data.html_url;
+            })
+            .catch(error => console.error('Erro ao buscar dados do GitHub:', error));
+    } catch (err) {
+        console.error('Erro no script principal:', err);
     }
 });
-
-
-// Example of fetching GitHub profile data using the Fetch API
-    
-        // document.addEventListener('DOMContentLoaded', function() {
-        //     const nameElement = document.querySelector('#name');
-        //     const usernameElement = document.querySelector('#username');
-        //     const avatarElement = document.querySelector('#avatar');
-        //     const reposElement = document.querySelector('#repos');
-        //     const followersElement = document.querySelector('#followers');
-        //     const followingElement = document.querySelector('#following');
-        //     const linkElement = document.querySelector('#profile-link');
-        //     try {
-        //          fetch('https://api.github.com/users/maxh33')
-        //              .then(function(res) {
-        //                  if (!res.ok) {
-        //                      throw new Error('Network response was not ok ' + res.statusText);
-        //                  }
-        //                  return res.json();
-        //             })
-        //             .then(function(data) {
-        //                 if (nameElement) nameElement.innerText = data.name;
-        //                 if (usernameElement) usernameElement.innerText = data.login;
-        //                 if (avatarElement) avatarElement.src = data.avatar_url;
-        //                 if (followingElement) followingElement.innerText = data.following;
-        //                 if (followersElement) followersElement.innerText = data.followers;
-        //                 if (reposElement) reposElement.innerText = data.public_repos;
-        //                 if (linkElement) linkElement.href = data.html_url;
-        //             })
-        //             .catch(function(err) {
-        //                 console.error('Error fetching data:', err.message);
-        //             });
-        //      } catch (error) {
-        //          console.error('Error fetching data:', error.message);
-        //      }
-        // });
