45d673d5c0815f5bbc850a7c6a1b368698e97441
WDebon <robielfelix@gmail.com>
2025-02-06 13:34:59 -0300

    Agenda!!

---
+++ src/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
+import Home from './pages/Home'
+import CriarContato from './pages/CriarContato'
+import GlobalStyle from './globals'
+// import reportWebVitals from './reportWebVitals'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/criar-contato',
+    element: <CriarContato />
+  }
+])
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+)
+
+// reportWebVitals()
