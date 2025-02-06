aff49a164c44908ae496a3047f2f7ad95441851f
WDebon <robielfelix@gmail.com>
2025-02-06 13:41:12 -0300

    Testando

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
-import filterReducer from './reducers/filter'
-import contactReducer from './reducers/contact'
+import {
+  combineReducers,
+  configureStore,
+  PreloadedState
+} from '@reduxjs/toolkit'
 
-const store = configureStore({
-  reducer: { contact: contactReducer, filter: filterReducer }
+import carrinhoReducer from './reducers/carrinho'
+
+import api from '../services/api'
+
+const rootReducer = combineReducers({
+  carrinho: carrinhoReducer,
+  [api.reducerPath]: api.reducer
 })
 
-export type RootReducer = ReturnType<typeof store.getState>
-export default store
+export function configuraStore(preloadedState?: PreloadedState<RootState>) {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(api.middleware),
+    preloadedState
+  })
+}
+
+export type RootState = ReturnType<typeof rootReducer>
+export type AppStore = ReturnType<typeof configuraStore>
